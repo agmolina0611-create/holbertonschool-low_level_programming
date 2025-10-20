d4e20630041da98db1a50dd017ee61bbdcd29ba2
Abner Molina <agmolina0611@gmail.com>
2025-10-20 11:21:00 -0400

    Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 0 to 9 followed by a newline
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
