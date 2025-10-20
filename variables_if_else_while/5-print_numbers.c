f8d4049aaf1ba9b2a0040e54b62272fe84750cb8
Abner Molina <agmolina0611@gmail.com>
2025-10-20 09:22:47 -0400

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 0 through 9
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
