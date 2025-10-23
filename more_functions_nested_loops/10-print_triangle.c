ff813c5801aa5219d740440e588fc828c2f4604f
Abner Molina <agmolina0611@gmail.com>
2025-10-23 12:10:31 -0400

    Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a right-aligned triangle of '#' characters
+ * @size: if size is 0 or less, the function should print only a new line
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, sp, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (sp = 0; sp < size - row; sp++)
+			_putchar(' ');
+		for (hash = 0; hash < row; hash++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
