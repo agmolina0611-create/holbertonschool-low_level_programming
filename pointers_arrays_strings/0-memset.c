d170f4ba5f7eb66c14285b6e9cabef2c5e9347c9
Abner Molina <agmolina0611@gmail.com>
2025-10-29 13:54:16 -0400

    memset

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to the memory area to fill
+ * @b: byte value to set
+ * @n: number of bytes to set
+ * Return: pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+
+	return (s);
+}
